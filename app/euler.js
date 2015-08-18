1ca353748d45f0804cf8e7115e04c0d9d70be909
Mark Michaels <msquarednet@gmail.com>
2015-08-18 20:59:55 +0000

    added isNumberZeroModFrom1thruX(number.x)

--- app/euler.js
+++ app/euler.js
@@ -13,20 +13,47 @@ var Euler005 = {
     },
     getAllFactors: function(num) {  //from 1 to num
         var arr = [];
+        var index = 0;
         for (var i=num; i>0; i--) {
-            arr.push.apply(arr, Euler005.getFactors(i));
+            arr.push(i);
         } 
-        //return arr;
-        function onlyUnique(value, index, self) { 
-            return self.indexOf(value) === index;
-        }
-        arr = arr.filter( onlyUnique ).sort();
+        for (var i=num; i>0; i--) {
+            arr.push(i);
+        } 
+        // var temparr;
+        // for (var i=num; i>0; i--) {
+        //     temparr = Euler005.getFactors(i);
+        //     console.log(temparr);
+        //     for (var j=0, l=temparr.length; j<l; j++) {
+        //         if (arr.indexOf(temparr[j])==-1) {arr.push(temparr[j])};
+        //     }
+        //     //arr.push.apply(arr, Euler005.getFactors(i));
+        // } 
         return arr;
+        
+        // function onlyUnique(value, index, self) { 
+        //     return self.indexOf(value) === index;
+        // }
+        // arr = arr.filter( onlyUnique ).sort();
+        // return arr;
         //well, that was dumb...
+    },
+    isNumberZeroModFrom1thruX: function(number, x) {    //should skip 1....
+        if (number && x) {
+            if (isNaN(number)) {number=1};  if (isNaN(x)) {x=1};
+            for (var i=1; i<=x; i++) {
+                if (number%i===0) {console.log("TRUE: "+number+"%"+i+"===0")}
+                else {
+                    console.log("FALSE: "+number+"%"+i+"===0")
+                    break;  return false;
+                }
+            }
+            return true;
+        } else {return false;}
     }
     
 };
 console.log("FOOBAR!!!!!!!!!!!!!!!!!!!!!");
 
-console.log(Euler005.getAllFactors(20));
-
+//console.log(Euler005.getAllFactors(6));
+console.log("is zero-mod for all: " + Euler005.isNumberZeroModFrom1thruX(2520,10));
