9e4a2ea01a14680986eba8a9d4f738876c336304
Mark Michaels <msquarednet@gmail.com>
2015-08-19 19:24:25 +0000

    finds the correct answer in about 20 seconds

--- app/euler.js
+++ app/euler.js
@@ -42,29 +42,35 @@ var Euler005 = {
         if (number && x) {
             if (isNaN(number)) {number=1};  if (isNaN(x)) {x=1};    //greater than zero?
             for (var i=1; i<=x; i++) {
-                if (number%i===0) {console.log("TRUE: "+number+"%"+i+"===0")}
+                if (number%i===0) {/*console.log("TRUE: "+number+"%"+i+"===0")*/}
                 else {
-                    console.log("FALSE: "+number+"%"+i+"===0")
-                    return false;   
-                    break;
+                    //console.log("FALSE: "+number+"%"+i+"===0")
+                    return false;   //break;
                 }
             }
             return true;
         } else {return false;}
     },
-    findSmallestNumberZeroModFrom1thruX: function(x) {    //should skip 1(?)....
+    findSmallestNumberZeroModFrom1thruX: function(x) {    //should skip 1(?).... 
         if (x) {
             if (isNaN(x)) {x=1};
             x = Math.abs(x);
             var worky = false;
+            var starttime = new Date().getTime();       //console.log(starttime);
             
             var number = x;
             do {
                 worky = Euler005.isNumberZeroModFrom1thruX(number,x);
-                if (worky) {console.log("---------------------------WORKY!!!!!: "+number); return number}
-                else {console.log("------------------NO worky: " + number)};
+                if (worky) {
+                    var endtime = new Date().getTime(); //console.log(endtime);
+                    console.log("---------------------------WORKY!!!!!: "+number); 
+                    console.log(endtime-starttime+" milliseconds");
+                    return number;
+                    
+                } else {/*console.log("------------------NO worky: " + number)*/};
                 number++;
             } while (!worky);
+            
             
         } else {return 0;}
     }
@@ -76,8 +82,10 @@ console.log("FOOBAR!!!!!!!!!!!!!!!!!!!!!");
 //console.log("is zero-mod for all: " + Euler005.isNumberZeroModFrom1thruX(2520,10));
 //console.log("is zero-mod for all: " + Euler005.isNumberZeroModFrom1thruX(232792560,20));
 
+
 // console.log("smallest zero-mod for 1-"+4+" is: " + Euler005.findSmallestNumberZeroModFrom1thruX(4));
 // console.log("smallest zero-mod for 1-"+10+" is: " + Euler005.findSmallestNumberZeroModFrom1thruX(10));
-console.log("smallest zero-mod for 1-"+11+" is: " + Euler005.findSmallestNumberZeroModFrom1thruX(11));
-// console.log("smallest zero-mod for 1-"+10+" is: " + Euler005.findSmallestNumberZeroModFrom1thruX(10));
+// console.log("smallest zero-mod for 1-"+11+" is: " + Euler005.findSmallestNumberZeroModFrom1thruX(11));
+//console.log("smallest zero-mod for 1-"+15+" is: " + Euler005.findSmallestNumberZeroModFrom1thruX(15));
+ console.log("smallest zero-mod for 1-"+20+" is: " + Euler005.findSmallestNumberZeroModFrom1thruX(20));
 
